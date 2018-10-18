e054f7fd3428cc092f80d342de20af2cb5271541
Michael Novotny <manovotny@gmail.com>
2018-10-18 17:01:53 -0500

    Adds initial codebase. (#1)

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['get-off-my-lawn', 'prettier'],
+    rules: {
+        'arrow-body-style': 0,
+        'react/jsx-closing-bracket-location': 0,
+        'react/jsx-closing-tag-location': 0,
+        'react/jsx-max-props-per-line': 0
+    }
+};
